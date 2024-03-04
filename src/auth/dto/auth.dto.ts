9d9ce6e71c1262d4909bf317b5061ae76ccb047c
Valeriy Danets <dantesvv@gmail.com>
2024-03-04 17:16:30 +0200

    Were implemented new methods into UserService

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,9 +1,19 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import {
+	IsEmail,
+	IsNotEmpty,
+	IsOptional,
+	IsString,
+	MinLength,
+} from 'class-validator';
 
 export class AuthDTO {
 	@IsEmail()
 	@IsNotEmpty()
 	email: string;
+
+	@IsString()
+	@IsOptional()
+	name?: string;
 
 	@MinLength(6, { message: 'Password must contains at least 6 characters' })
 	@IsString()
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,7 @@ import { AuthDTO } from 'src/auth/dto/auth.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 import * as argon from 'argon2';
+import { startOfDay, subDays } from 'date-fns';
 
 @Injectable()
 export class UserService {
@@ -40,5 +41,66 @@ export class UserService {
 				password: hash,
 			},
 		});
+	}
+
+	async update(id: string, dto: AuthDTO) {
+		let data = dto;
+		if (dto.password) {
+			data = { ...dto, password: await argon.hash(dto.password) };
+		}
+		return this.prisma.user.update({
+			where: {
+				id,
+			},
+			data,
+		});
+	}
+
+	async getProfile(id: string) {
+		const profile = await this.getById(id);
+
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const { password, ...user } = profile;
+
+		const totalTasks = profile.tasks.length;
+
+		// Todo Task.servise
+		const completedTasks = await this.prisma.task.count({
+			where: {
+				userId: id,
+				isCompleted: true,
+			},
+		});
+
+		const todayStart = startOfDay(new Date());
+		const weekStart = startOfDay(subDays(new Date(), 7));
+
+		const todayTasks = await this.prisma.task.count({
+			where: {
+				userId: id,
+				createdAt: {
+					gte: todayStart.toISOString(),
+				},
+			},
+		});
+
+		const weekTasks = await this.prisma.task.count({
+			where: {
+				userId: id,
+				createdAt: {
+					gte: weekStart.toISOString(),
+				},
+			},
+		});
+
+		return {
+			user,
+			statistics: [
+				{ label: 'Total', value: totalTasks },
+				{ label: 'Completed Tasks', value: completedTasks },
+				{ label: 'Today Tasks', value: todayTasks },
+				{ label: 'Week Tasks', value: weekTasks },
+			],
+		};
 	}
 }
