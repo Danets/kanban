4e54c2565f3e57110c5ebce0975e2e38c75307c5
Valeriy Danets <dantesvv@gmail.com>
2024-03-05 15:22:45 +0200

    Has been changed settings of main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,15 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	app.setGlobalPrefix('api');
 	app.use(cookieParser());
-	await app.listen(4000);
+	app.enableCors({
+		origin: ['http://localhost:3000'],
+		credentials: true,
+		exposedHeaders: 'set-cookie',
+	});
+
+	await app.listen(5000);
 }
 bootstrap();
