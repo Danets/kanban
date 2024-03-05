4ed92b4ed27e8bac86674703b6081fbf93cc5de8
Valeriy Danets <dantesvv@gmail.com>
2024-03-05 15:24:35 +0200

    Was implemented UserController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import {
+	Body,
+	Controller,
+	Get,
+	HttpCode,
+	HttpStatus,
+	Put,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common';
 
-@Controller('user')
+import { UserService } from './user.service';
+import { AuthDTO } from 'src/auth/dto/auth.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { GetUser } from 'src/auth/decorators/user.decorator';
+
+@Controller('user/profile')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+	constructor(private readonly userService: UserService) {}
+
+	@Get()
+	@HttpCode(HttpStatus.OK)
+	@Auth()
+	getProfile(@GetUser('id') id: string) {
+		return this.userService.getProfile(id);
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Put()
+	@HttpCode(HttpStatus.ACCEPTED)
+	@Auth()
+	async update(@GetUser('id') id: string, @Body() dto: AuthDTO) {
+		return this.userService.update(id, dto);
+	}
 }
