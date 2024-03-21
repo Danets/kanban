6f5662857561dc90bdd67a473f0f409571d1cea5
Valeriy Danets <dantesvv@gmail.com>
2024-03-21 11:28:32 +0200

    Was implemented AdminPage

--- client/src/app/admin/page.tsx
+++ client/src/app/admin/page.tsx
@@ -1,6 +1,18 @@
+import type { Metadata } from 'next'
+
+import { NO_INDEX_PAGE } from '@/constants/seo'
+
+import { AdminClient } from './AdminClient'
+
+export const metadata: Metadata = {
+	title: 'Admin Page',
+	...NO_INDEX_PAGE
+}
+
 export default function AdminPage() {
-    return (
-      <h1>Admin</h1>  
-    );
-  }
-  
+	return (
+		<>
+			<AdminClient />
+		</>
+	)
+}
